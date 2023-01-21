85929bbb097b42ce7610eb79c90818a3f316319a
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2023-01-21 14:42:07 +1100

    commit tree 0aaec0de190214e17780374226993cd675134e18

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+  
+  attr_reader :side1, :side2, :side3
+
+  def initialize (side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+
+    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
+      raise TriangleError
+    elsif @side1 >= @side2 + @side3 || @side2 >= @side1 + @side3 || @side3 >= @side1 + @side2
+      raise TriangleError
+
+    else
+
+      if @side1 == @side2 && @side2 == @side3
+        :equilateral
+      elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+
+  end
+  
 end
+
+t1 = Triangle.new(3,2,3)
+pp t1.kind
